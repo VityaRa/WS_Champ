83e1be42c701b1442e731956ebf736f5b5f4fba4
VityaRa <blackops2in@mail.ru>
2021-03-05 21:19:21 +0300

    init: created form to check responses

---
+++ Forma/Forma/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Forma
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            string answer = wc.DownloadString("https://localhost:44372/api/values/1");
+
+            Region rg = new Region();
+            rg = JsonConvert.DeserializeObject<Region>(answer);
+            ANS.Text = rg.capital;
+
+            answer = wc.DownloadString("https://localhost:44372/api/values");
+            var rgList = new List<Region>();
+            rgList = JsonConvert.DeserializeObject<List<Region>>(answer);
+            DG.ItemsSource = rgList;
+        }
+    }
+}
---
+++ Forma/Forma/Region.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forma
+{
+    public class Region
+    {
+        public int code { get; set; }
+        public string name { get; set; }
+        public string capital { get; set; }
+        public string district { get; set; }
+        public Region() { }
+        public Region(int code, string name, string capital, string district)
+        {
+            this.code = code;
+            this.name = name;
+            this.capital = capital;
+            this.district = district;
+        }
+
+        public static string SelectRegionCommand()
+        {
+            return "SELECT * FROM region";
+        }
+    }
+}
